c4e6dd5a6ef7a0afd44933c1bf26e9607bc5d598
KuldeepSoniSwapnil <49443497+SoniSSk@users.noreply.github.com>
2021-12-30 19:53:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 
 function App() {
+  const myStyle={
+    
+    width:'203%',
+    height:'200px',
+    };
   const [state, setState] = useState(20)
   const [state1, setState1] = useState("")
 const handleCharLimit = (event) =>{
@@ -18,14 +23,21 @@ else{
 }
 const [state2, setState2] = useState(50);
 const handlestate2 = (event) =>{
+ 
   setState2(event.target.value);
+}
+const [state3, setState3] = useState(50);
+const handlestate3 = (event) =>{
+ 
+  setState3(event.target.value);
 }
 
   return (
     <div>
-      <p>Char Limit:-<input value={state} onChange={handleCharLimit} ></input> <input ></input>  
-      <input value={state1} onChange={handlestate1} ></input><input type ="range" min ="1" max="100" value ={state2} ></input></p>
-      <div style={{background: "red"}}>{state1}</div>
+      <p>Char Limit:-<input value={state} onChange={handleCharLimit} ></input> <input type="color" value={state3} onChange={handlestate3}></input>  
+      <input value={state1} onChange={handlestate1} ></input><input type ="range" min ="1" max="100" value ={state2} onChange={handlestate2} ></input></p>
+      <div style={{background: "red", }}><p>{state1}</p></div>
+    
      
     </div>
   )
